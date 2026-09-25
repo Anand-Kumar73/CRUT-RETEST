8c3722fab3e10961d8e9aa17352ecd1f6e51cc5f
Anand kumar <anandk26771@gmail.com>
2026-09-25 21:25:22 +0530

    fix cors

--- frontend/src/services/profile.service.jsx
+++ frontend/src/services/profile.service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/profile/users";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getProfileDetails = async (id) => {
   const response = await axios.get(`${API_URL}/${id}`);
